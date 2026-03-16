80263129b5f46bf5a29ecad4eb72494d1bbb6287
yeong-mi <dudal010427@gmail.com>
2026-03-16 17:59:47 +0900

    Main페이지 Best섹션 렌더링 및 UI 추가 수정 완료

--- src/components/common/mainpage/BestpdLg.jsx
+++ src/components/common/mainpage/BestpdLg.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Best from '../badge/Best';
 import New from '../badge/New';
 import Exclusive from '../badge/Exclusive';
-import './BestboxSm.scss';
+import './BestpdLg.scss';
 
-const BestboxSm = ({ product }) => {
+const BestpdLg = ({ product }) => {
     // 상품 객체가 넘어오지 않았을 때 임시 값 (디자인 테스트용)
     const productData = product || {
         name: "제품명 들어가는 공간입니다.",
@@ -15,7 +15,7 @@ const BestboxSm = ({ product }) => {
     const imageUrl = productData.variants && productData.variants[0] ? productData.variants[0].image : "https://kr.aesop.com/dw/image/v2/AARM_PRD/on/demandware.static/-/Sites-aesop-master-catalog/default/dwd34d3c3d/images/products/SK67/4936968889721/4936968889721_1.png";
 
     return (
-        <div className="bestboxSm">
+        <div className="bestpdLg">
             <div className="badges">
                 {productData.badge.includes('Best') && <Best />}
                 {productData.badge.includes('New') && <New />}
@@ -29,4 +29,4 @@ const BestboxSm = ({ product }) => {
     );
 };
 
-export default BestboxSm;
+export default BestpdLg;
