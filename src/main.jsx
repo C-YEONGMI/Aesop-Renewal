92ee60d6a9f5ba9bbde66fbcdbf8d0f905c228f4
C-YEONGMI <dudal010427@gmail.com>
2026-03-22 23:34:32 +0900

    feat: initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import './globals.scss'
 import App from './App.jsx' // Note: This will be updated by bash command later, or I should update it now.
 
